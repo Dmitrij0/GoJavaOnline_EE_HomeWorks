2292a150406342d764a12e02fb556a0c3e7b58c0
Dmitrij0 <dmitrijlenchuk@gmail.com>
2016-09-20 10:28:51 +0300

    Deployment Fix

--- Module08/src/main/java/com/gojavaonline3/dlenchuk/ee/module08/ClockServlet.java
+++ Module08/src/main/java/com/gojavaonline3/dlenchuk/ee/module08/ClockServlet.java
@@ -1,6 +1,19 @@
 package com.gojavaonline3.dlenchuk.ee.module08;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 public class ClockServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Current Time is " + new Date());
+    }
+
 }
